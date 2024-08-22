28b89b6f8f5be964a2a7f7dfbb2e9469f7725319
IAmHesham <www.hichamhot@gmail.com>
2024-08-22 14:06:48 +0100

    ff

--- main.cpp
+++ main.cpp
@@ -3,14 +3,15 @@
 #include <iostream>
 #include <ostream>
 
-int main (int argc, char *argv[]) {
-	Scanner::setup_keywords();
-
-	std::string code = ". ; : & && | || ~ ~= < <= <<  > >= >> $hhhh";
-	Scanner scanner(code);
-	TokenList tokens = scanner.scan();
-	for (auto& token : tokens) {
-		std::cout << token.to_string() << std::endl;
-	}
-	return 0;
+int main(int argc, char *argv[])
+{
+    Scanner::setup_keywords();
+    std::string code = ". ; : & && | || ~ ~= < <= <<  > >= >> $hhhh";
+    Scanner scanner(code);
+    TokenList tokens = scanner.scan();
+    for (auto &token : tokens)
+    {
+        std::cout << token.to_string() << std::endl;
+    }
+    return 0;
 }
